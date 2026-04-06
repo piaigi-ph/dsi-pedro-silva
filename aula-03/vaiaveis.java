a2c4be0dfba441f508ac0103750c3d3b29b25934
pedro henrique <pedro260110@gmail.com>
2026-04-06 09:08:28 -0300

    adiciona variáveis e lógica de cálculo no exemplo de Java

--- aula-03/vaiaveis.java
+++ aula-03/vaiaveis.java
@@ -5,5 +5,10 @@ public class vaiaveis {
         string msgteste;
         msgteste = "oie";
         System.out.println(mynum + "" + msgteste);
+        int n1, n2, n3;
+        n1 = n2 +n3;
+        n3 = 5;
+        n1 = 10;
+        System.out.println(n2);
     }
 }
