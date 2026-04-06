4776b38ed030a1af9bdf94da3d3496d210f533de
pedro henrique <pedro260110@gmail.com>
2026-04-06 09:04:50 -0300

    adiciona arquivos de exemplo em Java, HTML e CSS, incluindo README com instruções de configuração do Java
    Add example Java and HTML/CSS files with setup instructions
    
    Introduces sample files for Java and web development, including Java starter code, multiple styled HTML activities, and example assets.
    Updates project structure and README to help users set up Java, promoting easier onboarding and experimentation with different web layouts.

---
+++ aula-03/vaiaveis.java
@@ -0,0 +1,9 @@
+public class vaiaveis {
+    public static void main(String[] args) {
+        int mynum;
+        mynum = 15;
+        string msgteste;
+        msgteste = "oie";
+        System.out.println(mynum + "" + msgteste);
+    }
+}
